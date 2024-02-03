9800773a520691053a72159dbedf64a3570d460e
lumban-stephen <stephenlumban@gmail.com>
2024-02-03 20:25:02 +0800

    add login, dept folder

---
+++ src/pages/dept/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dept: React.FC = () => {
+  return (
+    <div>
+      <h1>Dept Page</h1>
+      {/* Add your login page content here */}
+    </div>
+  );
+}
+
+export default Dept;
