9afed3ee0e7319869911c21c266344987d370dc0
John Michael <johnmichaellim041202@gmail.com>
2024-02-05 23:14:20 +0800

    add applications folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import Login from './pages/login';
 import './App.css';
+import Application from './pages/application';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Login></Login>
+        <Application></Application>
       </header>
     </div>
   );
--- src/pages/application/index.tsx
+++ src/pages/application/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const Login: React.FC = () => {
+const Application: React.FC = () => {
   return (
     <div>
-      <h1>Login Page</h1>
-      <h2>University Of Cebu - Lapu-Lapu and Mandaue</h2>
+      <h1>Application Page</h1>
+      <h2>Hallu</h2>
       <h3>WSMS: Working Scholar Management System</h3>
       {/* Add your login page content here */}
     </div>
   );
 }
 
-export default Login;
+export default Application;
