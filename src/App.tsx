20137c424f54599b0c1667a1ac06ff07cf870dd0
lumban-stephen <stephenlumban@gmail.com>
2024-02-27 22:54:50 +0800

    add 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,12 @@
 import React from 'react';
 import './App.css';
 import Notfound from './pages/notfound';
-import { Box, Button, Modal, Typography } from '@mui/material';
-import { purple } from '@mui/material/colors';
-
-const primary = purple[500]; // #f44336
 
 function App() {
   return (
-    <Modal
-      open={open}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column', 
-        minHeight: '100vh',
-        backgroundColor: primary,
-      }}
-    >
-      <Typography variant="h1" style={{ color: 'white' }}>
-        404
-      </Typography>
-      <Typography variant="h6" style={{ color: 'white' }}>
-        The page you’re looking for doesn’t exist.
-      </Typography>
-      <Button variant="contained">Back Home</Button>
-    </Box>
-    </Modal>
-    
+    <div>
+      <Notfound/>
+    </div>
   );
 }
 
--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material';
+import { purple } from '@mui/material/colors';
+
+const primary = purple[500]; // #f44336
 
 const Notfound: React.FC = () => {
   return (
     <div>
-      <h1>Not Found</h1>
-      {/* Add your login page content here */}
+      <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '100vh',
+        backgroundColor: primary,
+      }}
+    >
+      <Typography variant="h1" style={{ color: 'white' }}>
+        404
+      </Typography>
+    </Box>
     </div>
   );
 }
