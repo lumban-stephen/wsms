b9b2b4a11007c8525139a15e9151cb923ce63312
lumban-stephen <stephenlumban@gmail.com>
2024-02-27 22:39:03 +0800

    add notfound folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Signup from './pages/signup';
+import Notfound from './pages/notfound';
+import { Box, Button, Modal, Typography } from '@mui/material';
+import { purple } from '@mui/material/colors';
+
+const primary = purple[500]; // #f44336
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <Signup/>
-      </header>
-    </div>
+    <Modal
+      open={open}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column', 
+        minHeight: '100vh',
+        backgroundColor: primary,
+      }}
+    >
+      <Typography variant="h1" style={{ color: 'white' }}>
+        404
+      </Typography>
+      <Typography variant="h6" style={{ color: 'white' }}>
+        The page you’re looking for doesn’t exist.
+      </Typography>
+      <Button variant="contained">Back Home</Button>
+    </Box>
+    </Modal>
+    
   );
 }
 
---
+++ src/pages/notfound/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Notfound: React.FC = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      {/* Add your login page content here */}
+    </div>
+  );
+}
+
+export default Notfound;
