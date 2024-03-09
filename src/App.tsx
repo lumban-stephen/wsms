94fe84437002f9afdcfec4ce63c333a2c1d74a6c
unknown <Admin@CISCOLAB018.CSSERVER.local>
2024-03-09 10:38:41 +0800

    add navbar for ws

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import Login from './pages/login';
+import  { Wsnavbar }  from './components/navbar-ws';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Wsnavbar/>
         <Login></Login>
       </header>
     </div>
