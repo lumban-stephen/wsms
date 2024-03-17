31fbbd82c153deb409b7a2e98fc180af25f7d78d
lumban-stephen <stephenlumban@gmail.com>
2024-03-17 13:45:18 +0800

    update navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import Login from './pages/login';
 import './App.css';
+import NavBarDept from "./components/navbar-dept";
 
 function App() {
+  const [activeTab, setActiveTab] = useState('Announcements');
+
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <Login></Login>
+        <NavBarDept activeTab={activeTab} handleTabChange={handleTabChange} />
       </header>
     </div>
   );
