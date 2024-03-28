6cdba72d402805ac9580cc69ac3501f7e780abd1
lumban-stephen <stephenlumban@gmail.com>
2024-03-28 16:12:14 +0800

    update modal backdrop

--- src/components/create-post-modal.tsx
+++ src/components/create-post-modal.tsx
@@ -1,4 +1,4 @@
-import { Modal, Paper, TextField, Button, Box } from '@mui/material';
+import { Modal, Paper, TextField, Button, Box, Backdrop } from '@mui/material';
 import React from 'react';
 
 interface CreatePostModalProps {
@@ -16,9 +16,9 @@ const CreatePostModal: React.FC<CreatePostModalProps> = ({ open, onClose }) => {
   };
 
   return (
-    <Modal open={open} onClose={onClose}>
-      <Box sx={{ width: '800px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: '#fff', padding: '20px' }}>
-        <Paper sx={{ padding: '20px' }}>
+    <Modal open={open} onClose={onClose} BackdropComponent={Backdrop} BackdropProps={{ sx: { backgroundColor: 'rgba(70,130,180, 0.5)', borderRadius: '8px' } }}>
+      <Box sx={{ width: '800px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', backgroundColor: '#fff', padding: '20px', borderRadius: '8px' }}>
+        <Paper sx={{ padding: '20px', borderRadius: '8px' }}>
           <h1>Create an announcement</h1>
           <Box sx={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
             <TextField label="Title" fullWidth />
@@ -26,7 +26,7 @@ const CreatePostModal: React.FC<CreatePostModalProps> = ({ open, onClose }) => {
           </Box>
           <Box sx={{ display: 'flex', justifyContent: 'flex-end', marginTop: '20px' }}>
             <Button variant="contained" color="primary" onClick={handleCreatePost} sx={{ marginRight: '10px' }}>Submit</Button>
-            <Button variant="text" onClick={handleCancel}>Cancel</Button>
+            <Button variant="outlined" onClick={handleCancel}>Cancel</Button>
           </Box>
         </Paper>
       </Box>
