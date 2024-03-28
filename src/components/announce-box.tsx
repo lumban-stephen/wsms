fed261bffb1b0470819483a7019b8f024d0877f1
lumban-stephen <stephenlumban@gmail.com>
2024-03-28 15:02:35 +0800

    add announceBox

--- src/components/announce-box.tsx
+++ src/components/announce-box.tsx
@@ -3,20 +3,17 @@ import React from 'react';
 
 const AnnounceBox: React.FC = () => {
   return (
-    <Container maxWidth="lg">
       <Paper
         sx={{
           display: 'flex',
           justifyContent: 'center',
-          minHeight: '80px',
+          minHeight: '800px',
           marginTop: '20px', // Adjust this value to add the desired space
         }}
       >
         <h1 style={{ textAlign: 'center', margin: 0, padding: 0 }}>Dept Announcement Page</h1>
         {/* Add your login page content here */}
       </Paper>
-      
-    </Container>
   );
 };
 
