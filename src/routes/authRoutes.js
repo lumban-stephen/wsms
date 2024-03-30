763f7701a8f3e698b7b725dcad08f2ea271444ae
lumban-stephen <stephenlumban@gmail.com>
2024-03-30 08:55:16 +0800

    update authRoutes.js

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { Pool } = require('pg');
+
+const router = express.Router();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+router.post('/signup', async (req, res) => {
+  // Use the pool for database queries
+    const client = await pool.connect();
+    const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const query = 'INSERT INTO users (username, password) VALUES ($1, $2)';
+    const values = [username, hashedPassword];
+    await pool.query(query, values);
+
+    res.status(201).json({ message: 'User created successfully' });
+    client.release();
+  } catch (error) {
+    console.error('Error signing up user:', error);
+    res.status(500).json({ error: 'An error occurred while signing up' });
+  }
+});
+
+module.exports = router;
