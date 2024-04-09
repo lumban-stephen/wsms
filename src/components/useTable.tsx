a34a08caea4bd62537c10b98ecc9c36da10298b1
John Michael <johnmichaellim041202@gmail.com>
2024-04-09 23:48:14 +0800

    error

---
+++ src/components/useTable.tsx
@@ -0,0 +1,15 @@
+import { Table } from '@mui/material'
+import React from 'react'
+
+
+export default function UseTable(records, headCells) {
+
+    const TblContainer: React.FC = () => (
+        <Table>
+
+        </Table>
+    )
+  return (
+        TblContainer
+  )
+}
