1ece9fb141b31fe89fa860f18f437ac8cf046db6
lumban-stephen <stephenlumban@gmail.com>
2024-04-22 05:44:59 +0800

    removed React.Strictmode

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <div>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </div>
 );
