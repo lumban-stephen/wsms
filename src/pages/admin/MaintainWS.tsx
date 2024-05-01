294e14da17e5474cd55c981a97663229a108031e
John Michael <johnmichaellim041202@gmail.com>
2024-05-01 15:30:54 +0800

    transfer

---
+++ src/pages/admin/MaintainWS.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PaperContainer from '../../components/MWSPaper'
+import TableWSList from '../../components/MWSTable'
+
+export default function MainWS() {
+  return (
+    <div>
+      <PaperContainer>
+        <TableWSList></TableWSList>
+      </PaperContainer>
+    </div>
+  )
+}
