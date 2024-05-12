6280c926e004b1c8671c58439e611d325edcf017
lumban-stephen <stephenlumban@gmail.com>
2024-05-12 10:11:03 +0800

    add template.js

---
+++ src/routes/template.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { Pool } = require('pg');
+const router = express.Router();
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "wsms",
+  password: "10000max",
+  port: 5432,
+  secretkey: "M23y?_+Sb[ynL`_WBpp2LOzbOct&rq"
+});
+
+
+
+  module.exports = router;
